770aa1566f92c9a5247795890632eafd53be9259
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-05-26 18:39:24 +0200

    feat: implement table

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
+const booksTableBody = document.querySelector("[data-books-table-body");
+
 const books = [];
+const defaultBooks = [
+  new Book("Robert Jordan", "The Eye of the World", 782, true),
+  new Book("Naomi Novik", "In His Majesty's Service ", 833, true),
+  new Book("J.R.R. Tolkien", "The Lord of the Rings", 1178, false),
+];
 
 function Book(author, title, pageCount, hasBeenRead) {
   this.author = author;
@@ -19,11 +26,28 @@ function removeBook(index) {
   books.splice(index, 1);
 }
 
+function bookToHtml(book) {
+  return `
+    <tr>
+      <td>${book.author}</td>
+      <td>${book.title}</td>
+      <td class="numeric page-count">${book.pageCount}</td>
+      <td>${
+        book.hasBeenRead
+          ? '<span class="read">Yes</span>'
+          : '<span class="not-read">No</span>'
+      }</td>
+    </tr>`;
+}
+
 function displayBooks(books) {
-  for (const book of books) {
-    // TODO
-    console.log(book);
-  }
+  const booksHtml = books.map(bookToHtml).join("");
+  booksTableBody.innerHTML = booksHtml;
+}
+
+// Add default books to the collection
+for (const book of defaultBooks) {
+  storeBook(book);
 }
 
 displayBooks(books);
